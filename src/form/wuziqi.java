ecd5b09342822ee4b8eefe3a3d5fc239b9a6b4b7
zhoujump <zhouchujiede@126.com>
2023-02-20 09:16:47 +0800

    Changes

---
+++ src/form/wuziqi.java
@@ -0,0 +1,33 @@
+/*
+ * Created by JFormDesigner on Mon Feb 20 08:59:12 CST 2023
+ */
+
+package form;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author unknown
+ */
+public class wuziqi extends JFrame {
+    public wuziqi() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
