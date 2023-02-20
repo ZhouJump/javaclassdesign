83b329e1501911af7dd69b733217c32abc5083d3
zhoujump <zhouchujiede@126.com>
2023-02-20 10:10:23 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import com.formdev.flatlaf.*;
+import form.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        FlatDarculaLaf.install();
+
+        int[][] qipan = new int[19][19];//棋盘数组，0未落子 1落白字 2落黑子
+
+        new wuziqi().setVisible(true);
     }
 }
